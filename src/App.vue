69846c874eec3b3d6e876a55e6c302180b5ff95e
Arus Sayamyan <arus.sayamyan@codetime.am>
2023-11-21 14:30:28 +0400

    Change style of checkboxes

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@
       <div class="mainContainer__list" v-for="list of lists" :key="list.id"
            :class="{'mainContainer__list--visible' : list.isVisible}">
         <div class="mainContainer__header">
-          <img src="@/assets/arrowDown.svg" alt="" @click="makeListVisible(list.id)">
-          <input type="checkbox" :checked="list.checked" :id="list.id" @change="changeInputStatus(list.id)">
+          <img src="@/assets/arrowDown.svg" class="mainContainer__dropDownIcon" alt="" @click="makeListVisible(list.id)" :class="{'mainContainer__dropDownIcon--rotate' : list.isVisible}">
+          <input type="checkbox" :checked="list.checked" :id="list.id" @change="changeInputStatus(list.id)" class="mainContainer__checkbox">
           <label :for="list.id"> {{ list.listName }} </label>
         </div>
         <div class="mainContainer__items">
           <ul class="mainContainer__listItems">
             <li class="mainContainer__listItem" v-for="item in list.items" :key="item.id">
-              <div>
-                <input type="checkbox" :checked="item.checked" :id="item.id" @change="changeInputStatus(item.id)">
+              <div class="mainContainer__listItemRow">
+                <input type="checkbox" :checked="item.checked" :id="item.id" @change="changeInputStatus(item.id)" class="mainContainer__checkbox">
                 <label :for="item.id">{{ item.name }}</label>
               </div>
               <div class="mainContainer__props">
@@ -34,12 +34,10 @@
 
 <script setup>
 import {useStore} from "vuex";
-import {computed, ref} from "vue";
+import {computed} from "vue";
 import ItemLists from "@/components/ItemLists.vue";
 
 const store = useStore();
-const isVisible = ref(false)
-// const countVal = ref()
 const lists = computed(() => {
   return store.getters.getLists
 
@@ -63,24 +61,16 @@ function changeCountValue(event, id, changedInput) {
     id: id,
     changedInput: changedInput
   })
-  console.log(store.state)
 }
 </script>
 
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
 .mainContainer {
   display: flex;
   justify-content: space-around;
   gap: 20px;
+  margin-top: 100px;
 
   &__listWrapper {
     max-width: 500px;
@@ -122,6 +112,70 @@ function changeCountValue(event, id, changedInput) {
     border: none;
   }
 
+  &__listItem {
+    position: relative;
+
+    & label:before {
+      content: '';
+      -webkit-appearance: none;
+      background-color: transparent;
+      border: 2px solid #9ea5a9;
+      padding: 8px;
+      display: inline-block;
+      position: relative;
+      vertical-align: middle;
+      cursor: pointer;
+      margin-right: 5px;
+    }
+
+    & input:checked + label:after {
+      content: '';
+      display: block;
+      position: absolute;
+      top: 10px;
+      left: 7px;
+      width: 4px;
+      height: 12px;
+      border: solid #9ea5a9;
+      border-width: 0 2px 2px 0;
+      transform: rotate(45deg);
+    }
+  }
+
+  &__header {
+    position: relative;
+
+    & label:before {
+      content: '';
+      -webkit-appearance: none;
+      background-color: transparent;
+      border: 2px solid #9ea5a9;
+      padding: 8px;
+      display: inline-block;
+      position: relative;
+      vertical-align: middle;
+      cursor: pointer;
+      margin-right: 5px;
+    }
+
+    & input:checked + label:after {
+      content: '';
+      display: block;
+      position: absolute;
+      top: 4px;
+      left: 23px;
+      width: 4px;
+      height: 12px;
+      border: solid #9ea5a9;
+      border-width: 0 2px 2px 0;
+      transform: rotate(45deg);
+    }
+  }
+
+  &__checkbox {
+    display: none;
+  }
+
   &__count {
     width: 20px;
     border: none;
@@ -139,8 +193,37 @@ function changeCountValue(event, id, changedInput) {
     justify-content: center;
   }
 
+  &__dropDownIcon {
+    cursor: pointer;
+
+    &--rotate {
+      transform: rotate(-90deg);
+    }
+  }
+
+
   @media(max-width: 728px) {
     flex-direction: column;
   }
 }
+
+//.mainContainer__listItem
+//
+//
+//.mainContainer__listItem
+
+//.mainContainer__listItem label:before {
+//  content: '';
+//  -webkit-appearance: none;
+//  background-color: transparent;
+//  border: 2px solid #9ea5a9;
+//  padding: 8px;
+//  display: inline-block;
+//  position: relative;
+//  vertical-align: middle;
+//  cursor: pointer;
+//  margin-right: 5px;
+//}
+
+
 </style>
